076b726e7b1032ca47a3de5776f2e55f9178d97a
gauss963 <gauss963@gmail.com>
2024-09-30 11:58:19 +0800

    Get plotting working and label peak value

--- waveform.f90
+++ waveform.f90
@@ -1,11 +1,14 @@
-program waveform
+ program waveform
     implicit none
     integer :: n, i
     integer :: io_status, unit_num
     character(len = 100) :: line
     real, allocatable :: T(:), E1(:), E2(:), E3(:)
+    real :: peak_value1, peak_value2, peak_value3
+    integer :: peak_idx1, peak_idx2, peak_idx3
+    character(len=20) :: peak_str1, peak_str2, peak_str3
 
-
+    ! 讀取數據文件
     open(newunit=unit_num, file = "../data/seisdata.txt", status = "old", action = "read")
 
     n = 0
@@ -17,7 +20,6 @@ program waveform
     close(unit_num)
     allocate(T(n), E1(n), E2(n), E3(n))
 
-
     open(newunit=unit_num, file = "../data/seisdata.txt", status = "old", action = "read")
     do i = 1, n
         read(unit_num, *, iostat=io_status) T(i), E1(i), E2(i), E3(i)
@@ -25,40 +27,62 @@ program waveform
     end do
     close(unit_num)
 
+    peak_value1 = maxval(E1)
+    peak_value2 = maxval(E2)
+    peak_value3 = maxval(E3)
 
-    print *, n
+    peak_idx1 = maxloc(E1, dim=1)
+    peak_idx2 = maxloc(E2, dim=1)
+    peak_idx3 = maxloc(E3, dim=1)
 
-    print *, "Waveform of first 10 samples:"
-    do i = 1, min(10, n)
-        print *, T(i)
-    end do
+ 
+    write(peak_str1, '(F8.2)') peak_value1
+    write(peak_str2, '(F8.2)') peak_value2
+    write(peak_str3, '(F8.2)') peak_value3
 
-    do i = 1, min(10, n)
-        print *, E1(i), E2(i), E3(i)
-    end do
+    ! call pgopen('waveform_plot.png/PNG')
+    call pgopen('waveform_plot.ps/VCPS')
+    call pgsubp(1, 3)
 
-    call pgopen('waveform_plot.png/PNG')
-    call pgenv(minval(T(1:n)), maxval(T(1:n)), minval(E1(1:n)), maxval(E1(1:n)), 0, 1)
-    call pglabel('Time (T)', 'Amplitude', 'Waveform E1, E2, E3')
+    call pgsci(1) 
+    call pgslw(4)
+    call pgsch(1.5)
+    call pgscf(2)
 
-    ! 繪製 E1 波形
+    ! Plot E1
+    call pgsci(1)
+    call pgenv(minval(T), maxval(T), minval(E1), maxval(E1), 0, 1)
+    call pglab('Time (sec)', 'Amplitude', 'Waveform E1')
+    call pgsci(2)
     call pgline(n, T, E1)
 
-    ! 更改顏色繪製 E2
-    call pgsch(1.0)        ! 設定字體大小
-    call pgslw(2)          ! 設定線寬
-    call pgsci(3)          ! 設定顏色 (3 表示綠色)
+    ! 標註 E1 的峰值
+    call pgpt(1, [T(peak_idx1)], [peak_value1], 17)
+    call pgtext(T(peak_idx1), peak_value1 - 2, 'Peak='//trim(adjustl(peak_str1)))
+
+    ! Plot E2
+    call pgsci(1)
+    call pgenv(minval(T), maxval(T), minval(E2), maxval(E2), 0, 1)
+    call pglab('Time (sec)', 'Amplitude', 'Waveform E2')
+    call pgsci(3)
     call pgline(n, T, E2)
 
-    ! 更改顏色繪製 E3
-    call pgsci(4)          ! 設定顏色 (4 表示藍色)
+    ! 標註 E2 的峰值
+    call pgpt(1, [T(peak_idx2)], [peak_value2], 17)
+    call pgtext(T(peak_idx2), peak_value2 - 2, 'Peak='//trim(adjustl(peak_str2)))
+
+    ! Plot E3
+    call pgsci(1)
+    call pgenv(minval(T), maxval(T), minval(E3), maxval(E3), 0, 1)
+    call pglab('Time (sec)', 'Amplitude', 'Waveform E3')
+    call pgsci(4)
     call pgline(n, T, E3)
 
-    ! 關閉繪圖裝置
+    ! 標註 E3 的峰值
+    call pgpt(1, [T(peak_idx3)], [peak_value3], 17)
+    call pgtext(T(peak_idx3), peak_value3 - 2, 'Peak='//trim(adjustl(peak_str3)))
+
     call pgclos()
-
-
-
 
     deallocate(T, E1, E2, E3)
 end program waveform
